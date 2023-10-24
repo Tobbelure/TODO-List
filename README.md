84a7f0f91efee613694ad971154c709de7f05c0d
Tobbelure <sorensentobias06@gmail.com>
2023-10-24 15:01:30 +0200

    Add Buttons iIssue 5)
    Add list (issue 6)
    
    Add input (issue 4)
    
    Add color to the side
    
    Add placeholders

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # Added more code and more labels for people to write what tasks they want to make
 # Added headline fo people to write by their own what kind of list they want to make
 # Added variables for my input, box and list
+# Made buttons work with click function
--- script.js
+++ script.js
@@ -2,8 +2,43 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     // variabler for inputen, boksen og listen jeg laget i index filen
-    const OppgaveInput = document.getElementById("OppgaveInput");
-    const AddOppgave = document.getElementById("AddOppgave");
-    const OppgaveListe = document.getElementById("OppgaveListe");
+    const oppgaveInput = document.getElementById("oppgaveInput");
+    const addOppgaveButton = document.getElementById("addOppgaveButton");
+    const oppgaveListe = document.getElementById("oppgaveListe");
 
-})
+
+    // legger til klikk funksjon til knappen
+    addOppgaveButton.addEventListener("click", function () {
+        const oppgaveTekst = oppgaveInput.value.trim();
+
+
+
+    // sjekker om bruker har skrevet noe inn i oppgaveboksen eller om den er tom
+        if (oppgaveTekst !== ""/* sjekker om det som er skrevet er ikke lik en tom string*/ ) {
+            /* lager en ny liste i HTML */ const li = document.createElement("li");
+            li.innerHTML = `
+                <input type="checkbox" class="oppgave-checkbox">
+                <span>${oppgaveTekst}</span>
+                <button class ="Slett">Slett</button>
+                `;
+            
+            oppgaveListe.appendChild(li);
+            oppgaveInput.value = "";
+
+
+            // legger til en slettboks som fjerner en oppgave som bruker har laget
+            const slettKnapp = li.querySelector(".slett");
+            slettKnapp.addEventListener("click", function () {
+                li.remove();
+            });
+        }
+    });
+    // lager en linje gjennom oppgaver som er fullført/checked
+    oppgaveListe.addEventListener("change", function (event) {
+        if (event.target.classList.contains("task-checkbox")) {
+            const taskSpan = event.target.nextElementSibling;
+            taskSpan.style.textDecoration = event.target.checked ? "line-through" : "none";
+
+        }
+    });
+});
